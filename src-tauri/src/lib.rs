6e0dbea6027283c40e32c661866b8622feeb9a7c
JiangShao <41313321+1024dashen@users.noreply.github.com>
2025-02-22 16:28:56 +0800

    update lib rust from pakeplus

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,13 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
 mod command;
-use serde_json::json;
-use tauri::{menu::*, Manager, WindowEvent};
+use serde_json::{json, Error};
+use tauri::{menu::*, utils::config::WindowConfig, WindowEvent};
 use tauri_plugin_store::StoreExt;
+
+fn json_to_window_config(window_json: &str) -> Result<WindowConfig, Error> {
+    serde_json::from_str(window_json)
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -32,7 +36,6 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
@@ -45,38 +48,52 @@ pub fn run() {
             command::pakeplus::update_config_file,
             command::pakeplus::update_cargo_file,
             command::pakeplus::update_main_rust,
-            command::pakeplus::rust_lib_window,
             command::pakeplus::update_custom_js,
             command::pakeplus::content_to_base64,
             command::pakeplus::update_config_json,
             command::pakeplus::rust_main_window,
         ])
         .setup(|app| {
-            let window = app.get_webview_window("main").unwrap();
-            // This loads the store from disk
+            let app_handle = app.handle();
+            // get window size store
             let store = app.store("app_data.json")?;
             let window_size: Option<serde_json::Value> = store.get("window_size");
+            let mut window_json = r#"{"label":"resize","title":"Reszie","url":"https://juejin.cn/","userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36","width":800,"height":600,"theme":null,"resizable":true,"fullscreen":false,"maximized":false,"minWidth":400,"minHeight":300,"maxWidth":1920,"maxHeight":1080,"decorations":true,"transparent":false,"titleBarStyle":"Visible","visible":true,"focus":true,"closable":true,"minimizable":true,"maximizable":true,"alwaysOnTop":false,"alwaysOnBottom":false,"center":false,"skipTaskbar":false,"tabbingIdentifier":null,"parent":null,"dragDropEnabled":true,"browserExtensionsEnabled":false,"devtools":true,"contentProtected":false,"hiddenTitle":false,"incognito":false,"proxyUrl":null,"useHttpsScheme":false,"zoomHotkeysEnabled":false,"acceptFirstMouse":false}"#;
             if let Some(window_size) = window_size {
                 let size = window_size.as_object().unwrap();
                 let width = size["width"].as_f64().unwrap();
                 let height = size["height"].as_f64().unwrap();
-                window
-                    .set_size(tauri::PhysicalSize::new(width, height))
-                    .unwrap();
+                // use regex replace window_json "width":XXX,"height":XXX, to "width":width,"height":height,
+                let regex = Regex::new(r#""width":\d+,"height":\d+"#).unwrap();
+                window_json = regex.replace_all(
+                    window_json,
+                    &format!(r#""width":{},"height":{}"#, width, height),
+                );
             }
-
-            window.on_window_event(move |event| {
-                if let WindowEvent::Resized(size) = event {
-                    let _ = store.set(
-                        "window_size",
-                        json!({
-                            "width": size.width,
-                            "height": size.height
-                        }),
-                    );
+            match json_to_window_config(window_json) {
+                Ok(config) => {
+                    // println!("Parsed WindowConfig: {:?}", config);
+                    let main_window = tauri::WebviewWindowBuilder::from_config(app_handle, &config)
+                        .unwrap()
+                        .build()
+                        .unwrap();
+                    // listen window sizi change event
+                    main_window.on_window_event(move |event| {
+                        if let WindowEvent::Resized(size) = event {
+                            let _ = store.set(
+                                "window_size",
+                                json!({
+                                    "width": size.width,
+                                    "height": size.height
+                                }),
+                            );
+                        }
+                    });
                 }
-            });
-
+                Err(err) => {
+                    eprintln!("Failed to parse JSON: {}", err);
+                }
+            }
             Ok(())
         })
         .run(tauri::generate_context!())
